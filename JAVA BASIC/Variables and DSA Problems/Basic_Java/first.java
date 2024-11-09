f76f4ddf4beeb624d638fd7c58aa00da37119450
Mohammad Alim <146476807+alim034@users.noreply.github.com>
2024-11-09 11:10:39 +0530

    Add files via upload

---
+++ JAVA BASIC/Variables and DSA Problems/Basic_Java/first.java
@@ -0,0 +1,10 @@
+package Basic_Java;
+
+public class first 
+{
+    public static void main(String[] args)
+    {
+        System.err.println("Hello World");
+        System.out.println("Mohammad Alim");
+    }
+}
