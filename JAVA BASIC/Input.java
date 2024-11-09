83f379bdb0d136ad5b8c472d18f920cbc61191f2
Mohammad Alim <146476807+alim034@users.noreply.github.com>
2024-11-09 12:28:01 +0530

    Create Input.java

---
+++ JAVA BASIC/Input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Input 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        // Interger Value
+        System.out.println("Enter Any Integer: ");
+        int n = sc.nextInt();
+
+        // String values
+        System.out.println("Enter Name: ");
+        String s = sc.nextLine();
+    }
+}
