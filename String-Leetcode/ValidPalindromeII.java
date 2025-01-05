7898490444d5b764f9afa1e45427f4e66a2c7972
Mohammad Alim <146476807+alim034@users.noreply.github.com>
2025-01-05 18:54:58 +0530

    Add files via upload

---
+++ String-Leetcode/ValidPalindromeII.java
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/valid-palindrome-ii/description/
+
+public class ValidPalindromeII 
+{
+    public static void main(String[] args) 
+    {
+        String s = "abca";
+        System.out.println(validPalindrome(s));
+    }
+
+    public static boolean validPalindrome(String s) 
+    {
+        int left = 0;
+        int right = s.length() - 1;
+    
+        while (left < right) 
+        {
+                if (s.charAt(left) == s.charAt(right)) 
+                {
+                    left++;
+                    right--;
+                } 
+                else 
+                {
+                    return isPalindrome(s, left + 1, right)
+                        || isPalindrome(s, left, right - 1);
+                }
+        }
+  
+        return true;
+    }
+  
+    public static boolean isPalindrome(String s, int left, int right) 
+    {
+        while (left < right) 
+        {
+                if (s.charAt(left) == s.charAt(right)) 
+                {
+                    left++;
+                    right--;
+                }
+                else 
+                    return false;
+        }
+        return true;
+    }
+  
+}
